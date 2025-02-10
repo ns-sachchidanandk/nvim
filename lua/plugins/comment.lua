975f5d8956a9a4bdbda0400128ae4ac2d737c015
Sachchidanand Kumar <sachchidanandk@netskope.com>
2025-02-10 11:56:55 +0530

    update comments config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -6,19 +6,31 @@ return {
             require("Comment").setup({
                 padding = true, -- Add a space between comment and text
                 sticky = true, -- Keep cursor in place after commenting
-                ignore = "^$", -- Ignore empty lines when toggling
+                ignore = "^%s*$", -- Ignore empty/whitespace-only lines
                 toggler = {
                     line = "gcc", -- Toggle line comment
-                    block = "<leader>?", -- Toggle block comment
+                    block = "gbc", -- Toggle block comment
                 },
                 opleader = {
-                    line = "gcc", -- Operator pending mode for line comment
-                    block = "<leader>?", -- Operator pending mode for block comment
+                    line = "gc", -- Operator pending mode for line comment
+                    block = "gb", -- Operator pending mode for block comment
                 },
                 mappings = {
                     basic = true, -- Enable basic mappings
-                    extra = false, -- Disable extra mappings (like EOL commenting)
+                    extra = true, -- Enable extra mappings (e.g., comment end of line)
                 },
+                pre_hook = function(ctx)
+                    -- Example: integrate with ts-context-commentstring for JSX/TSX
+                    local U = require("Comment.utils")
+                    local ts_context = require("ts_context_commentstring.integrations.comment_nvim")
+                    return ts_context and ts_context.create_pre_hook()(ctx) or nil
+                end,
+                post_hook = function(ctx)
+                    -- Example: refresh diagnostics after commenting
+                    vim.defer_fn(function()
+                        vim.diagnostic.setloclist({ open = false })
+                    end, 100)
+                end,
             })
         end,
     }
