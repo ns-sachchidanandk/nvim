a3cdec624bac1a0659c7b749dd4dcf9da1d03294
Sachchidanand Kumar <sachchidanandk@netskope.com>
2025-02-10 11:47:04 +0530

    update lsp config keybinding for better usability

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -15,14 +15,25 @@ return {
                 })
             end
 
-            -- Keybindings for LSP (with vertical split support)
+            -- Function to check if the current buffer is already open in a split
+            local function open_or_focus_split(cmd)
+                local winid = vim.fn.bufwinid(vim.fn.expand("<cword>"))
+                if winid ~= -1 then
+                    vim.api.nvim_set_current_win(winid)
+                else
+                    vim.cmd(cmd)
+                end
+            end
+
+            -- Keybindings for LSP (smart split handling)
             local keymap = vim.keymap.set
-            keymap("n", "gd", "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", { desc = "Go to Definition (vsplit)" })
-            keymap("n", "gr", "<cmd>vsplit | lua vim.lsp.buf.references()<CR>", { desc = "Find References (vsplit)" })
-            keymap("n", "gD", "<cmd>vsplit | lua vim.lsp.buf.declaration()<CR>", { desc = "Go to Declaration (vsplit)" })
-            keymap("n", "gi", "<cmd>vsplit | lua vim.lsp.buf.implementation()<CR>", { desc = "Go to Implementation (vsplit)" })
+            keymap("n", "gd", function() open_or_focus_split("vsplit | lua vim.lsp.buf.definition()") end, { desc = "Go to Definition (smart vsplit)" })
+            keymap("n", "gr", function() open_or_focus_split("vsplit | lua vim.lsp.buf.references()") end, { desc = "Find References (smart vsplit)" })
+            keymap("n", "gD", function() open_or_focus_split("vsplit | lua vim.lsp.buf.declaration()") end, { desc = "Go to Declaration (smart vsplit)" })
+            keymap("n", "gi", function() open_or_focus_split("vsplit | lua vim.lsp.buf.implementation()") end, { desc = "Go to Implementation (smart vsplit)" })
             keymap("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
             keymap("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol" })
         end,
     },
 }
+
